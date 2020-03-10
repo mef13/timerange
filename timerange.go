b034d5c16f1208476b8ae78e645d2503cd476e92
mef <mef4mail@gmail.com>
2020-03-10 19:03:35 +0300

    Add inYear, inQuarter and ToSeconds

--- timerange.go
+++ timerange.go
@@ -1,12 +1,19 @@
 package timerange
 
-import "time"
+import (
+	"time"
+)
 
 type Time struct {
 	time.Time
 }
 
 type Duration int64
+
+type TimeDuration struct {
+	time time.Time
+	Duration
+}
 
 const (
 	Nanosecond  Duration = 1
@@ -31,10 +38,40 @@ func (t Time) DaysInMonth() int {
 	return 32 - nextMonth.Day()
 }
 
-func (t Time) InMonth() Duration {
-	return Duration(t.DaysInMonth()) * Day
+func (t Time) InMonth() TimeDuration {
+	return TimeDuration{
+		time:     t.Time,
+		Duration: Duration(t.DaysInMonth()) * Day,
+	}
 }
 
-func (d Duration) ToDays() int {
-	return int(d / Day)
+func (t Time) InYear() TimeDuration {
+	t1 := time.Date(t.Year(), 13, 1, 0, 0, 0, 0, time.UTC)
+	t2 := time.Date(t.Year(), 1, 1, 0, 0, 0, 0, time.UTC)
+	duration := t1.Sub(t2)
+	return TimeDuration{
+		time:     t.Time,
+		Duration: Duration(duration),
+	}
+}
+
+func (t Time) InQuarter() TimeDuration {
+	offset := (int(t.Month()) - 1) % 3
+	t1 := t.AddDate(0, 3-offset,0)
+	t2 := t.AddDate(0, -offset,0)
+	t1 = time.Date(t1.Year(), t1.Month(), 1, 0, 0, 0, 0, time.UTC)
+	t2 = time.Date(t2.Year(), t2.Month(), 1, 0, 0, 0, 0, time.UTC)
+	duration := t1.Sub(t2)
+	return TimeDuration{
+		time:     t.Time,
+		Duration: Duration(duration),
+	}
+}
+
+func (d TimeDuration) ToDays() int {
+	return int(d.Duration / Day)
+}
+
+func (d TimeDuration) ToSeconds() int {
+	return int(d.Duration / Second)
 }
--- timerange_test.go
+++ timerange_test.go
@@ -6,29 +6,80 @@ import (
 )
 
 type testTime struct {
-	time time.Time
-	daysInMounth int
+	time          time.Time
+	daysInMounth  int
+	daysInYear    int
+	daysInQuarter int
 }
 
 var samples = []testTime{
 	{
-		time: time.Date(2020, 2, 3, 2, 1, 10, 9, time.UTC),
-		daysInMounth: 29,
+		time:          time.Date(2020, 2, 3, 2, 1, 10, 9, time.UTC),
+		daysInMounth:  29,
+		daysInYear:    366,
+		daysInQuarter: 91,
 	},
 	{
-		time: time.Date(2020, 3, 3, 2, 1, 10, 9, time.UTC),
-		daysInMounth: 31,
+		time:          time.Date(2019, 3, 3, 2, 1, 10, 9, time.UTC),
+		daysInMounth:  31,
+		daysInYear:    365,
+		daysInQuarter: 90,
+	},
+	{
+		time:          time.Date(2019, 12, 3, 2, 1, 10, 9, time.UTC),
+		daysInMounth:  31,
+		daysInYear:    365,
+		daysInQuarter: 92,
+	},
+	{
+		time:          time.Date(2018, 11, 3, 2, 1, 10, 9, time.UTC),
+		daysInMounth:  30,
+		daysInYear:    365,
+		daysInQuarter: 92,
 	},
 }
 
-func TestDuration_ToDays(t *testing.T) {
-	for _, sample := range samples{
+func TestDurations_InMonth(t *testing.T) {
+	for _, sample := range samples {
 		daysInMonth := FromTime(sample.time).InMonth().ToDays()
 		if daysInMonth != FromTime(sample.time).DaysInMonth() {
 			t.Error("Invalid ToDays conversion")
 		}
 		if daysInMonth != sample.daysInMounth {
-			t.Error("Invalid days count")
+			t.Error("Invalid days in month count")
+		}
+
+		secondsInMonth := FromTime(sample.time).InMonth().ToSeconds()
+		if secondsInMonth != sample.daysInMounth*24*60*60 {
+			t.Error("Invalid seconds in month count")
+		}
+	}
+}
+
+func TestDurations_InYear(t *testing.T) {
+	for _, sample := range samples {
+		daysInYear := FromTime(sample.time).InYear().ToDays()
+		if daysInYear != sample.daysInYear {
+			t.Error("Invalid days in year count")
+		}
+
+		secondsInYear := FromTime(sample.time).InYear().ToSeconds()
+		if secondsInYear != sample.daysInYear*24*60*60 {
+			t.Error("Invalid seconds in year count")
+		}
+	}
+}
+
+func TestDurations_InQuarter(t *testing.T) {
+	for _, sample := range samples {
+		daysInQuarter := FromTime(sample.time).InQuarter().ToDays()
+		if daysInQuarter != sample.daysInQuarter {
+			t.Error("Invalid days in quarter count", sample.time, FromTime(sample.time).InQuarter().ToDays())
+		}
+
+		secondsInQuarter := FromTime(sample.time).InQuarter().ToSeconds()
+		if secondsInQuarter != sample.daysInQuarter*24*60*60 {
+			t.Error("Invalid seconds in quarter count")
 		}
 	}
 }
